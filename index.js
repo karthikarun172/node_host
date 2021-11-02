0ad51a4e1a3573c6ad612c5d200e52447bb08621
karthikarun172 <karthikarun172@gmail.com>
2021-11-02 10:53:38 +0530

    port change

--- index.js
+++ index.js
@@ -6,7 +6,8 @@ const app = express();
 app.get("/", async (req, res) => {
   res.send("welcome homies");
 });
+const PORT = process.env.PORT || 4000;
 
-app.listen(4000, () => {
+app.listen(PORT, () => {
   console.log("ho mama");
 });
